a696e6a5a24afeb10b3ed2aa8829a1c0cba62be0
LXX <13895167943@163.com>
2017-12-08 17:14:24 +0800

    把config-server和config-client注册到服务注册中心，部署多个config-server，config-client配置负载均衡，达到配置服务高可用

--- springCloudConfigServer/src/main/java/com/springcloud/configserver/demo/DemoApplication.java
+++ springCloudConfigServer/src/main/java/com/springcloud/configserver/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.springcloud.configserver.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer//@EnableConfigServer注解开启配置服务器的功能
+@EnableEurekaClient
 public class DemoApplication {
 
 	public static void main(String[] args) {
